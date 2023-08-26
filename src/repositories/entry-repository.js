aa1b35d28b9209a5771b7e05cbcd78a86f182b79
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-27 01:06:32 +0530

    add: method to delete an entry by id

--- src/repositories/entry-repository.js
+++ src/repositories/entry-repository.js
@@ -1,19 +1,37 @@
 const Entry = require('../models/entry');
 
+const matchId = (id) => (entry) => entry.getId() === id;
+const matchUserId = (userId) => (entry) => entry.getUserId() === userId;
+const entryToJSON = (entry) => entry.toJSON();
+
 class EntryRepository {
   #fs;
   #path;
+  #logger;
   #entries;
 
-  constructor(path, fs) {
+  constructor(path, fs, logger = console) {
     this.#fs = fs;
     this.#path = path;
+    this.#logger = logger;
   }
 
   #deserializeEntries() {
     const data = this.#fs.readFileSync(this.#path, 'utf-8');
     const rawEntries = JSON.parse(data || '[]');
     return rawEntries.map((entryInfo) => new Entry(entryInfo));
+  }
+
+  serializeEntries() {
+    return (this.#entries || []).map(entryToJSON);
+  }
+
+  #update() {
+    const entries = this.serializeEntries();
+
+    this.#fs.writeFile(this.#path, JSON.stringify(entries), (err) => {
+      if (err) this.#logger.log(err);
+    });
   }
 
   getAll() {
@@ -26,12 +44,20 @@ class EntryRepository {
     return this.#entries;
   }
 
+  findEntryOfId(id) {
+    return this.getAll().find(matchId(id));
+  }
+
+  deleteEntryOfId(id) {
+    const indexOfEntry = this.getAll().findIndex(matchId(id));
+    this.#entries.splice(indexOfEntry, 1);
+    this.#update();
+  }
+
   findEntries(options) {
     const { userId } = options;
-    const entries = this.getAll().filter(
-      (entry) => entry.getUserId() === userId
-    );
-    return entries.map((entry) => entry.toJSON());
+    const entries = this.getAll().filter(matchUserId(userId));
+    return entries.map(entryToJSON);
   }
 }
 
--- test/repositories/entry-repository-test.js
+++ test/repositories/entry-repository-test.js
@@ -86,4 +86,42 @@ describe('EntryRepository', () => {
       assert.deepStrictEqual(actualEntries, [secondEntry]);
     });
   });
+
+  describe('deleteEntryOfId', () => {
+    it('should delete the entry and update the database', (context) => {
+      const firstEntry = {
+        id: 0,
+        timeStamp: new Date().toUTCString(),
+        type: 'income',
+        amount: 800,
+        userId: 567,
+        category: 'grocery',
+        title: 'monday grocery',
+      };
+
+      const secondEntry = {
+        id: 1,
+        timeStamp: new Date().toUTCString(),
+        type: 'income',
+        amount: 800,
+        userId: 50,
+        category: 'grocery',
+        title: 'monday grocery',
+      };
+
+      const storedEntries = [firstEntry, secondEntry];
+
+      const fs = {
+        existsSync: context.mock.fn(() => true),
+        writeFile: context.mock.fn((path, content, callback) => callback()),
+        readFileSync: context.mock.fn(() => JSON.stringify(storedEntries)),
+      };
+
+      const entryRepo = new EntryRepository(null, fs);
+
+      entryRepo.deleteEntryOfId(0);
+
+      assert.deepStrictEqual(entryRepo.serializeEntries(), [secondEntry]);
+    });
+  });
 });
