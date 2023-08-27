f3ef9ff39ee3af2610873fa6d7be57e0d4af37e7
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-27 09:53:52 +0530

    update: server.js and app.js by injecting entryRepository and idGenerator

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+
 const { createApp } = require('./src/app');
+const EntryRepository = require('./src/repositories/entry-repository');
+const { generateId } = require('./src/id-generator');
 
 const main = () => {
-  const app = createApp();
+  const ENTRY_PATH = './data/entries.json';
+  const entryRepository = new EntryRepository(ENTRY_PATH, fs, console);
+
+  const idGenerator = generateId();
+
+  const app = createApp({ entryRepository, idGenerator });
   const PORT = 8000;
   app.listen(PORT, () => console.log('started listening on port', PORT));
 };
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { cookieParser } = require('./middlewares/cookie-parser');
 const { serveEntries, createEntry } = require('./handlers/entry-handlers');
+const { logger } = require('./middlewares/logger');
 
 const createApp = ({ entryRepository, idGenerator }) => {
   const app = express();
@@ -11,6 +12,7 @@ const createApp = ({ entryRepository, idGenerator }) => {
 
   app.use(cookieParser);
   app.use(express.json());
+  app.use(logger);
 
   app.get('/entries', serveEntries);
   app.post('/entries', createEntry);
