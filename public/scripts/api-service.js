9e71d7fe20c85f492b37808b2c1d5991c3c0d1a7
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-27 10:44:15 +0530

    add: api service to request for all entries and create an entry

---
+++ public/scripts/api-service.js
@@ -0,0 +1,19 @@
+const sendJsonRequest = (url, options, onResponse) => {
+  fetch(url, options)
+    .then((res) => res.json())
+    .then(onResponse);
+};
+
+const sendEntry = (entry, onResponse) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(entry),
+    headers: { 'content-type': 'application/json' },
+  };
+
+  sendJsonRequest('/entries', options, onResponse);
+};
+
+const getAllEntries = (onResponse) => {
+  sendJsonRequest('/entries', null, onResponse);
+};
