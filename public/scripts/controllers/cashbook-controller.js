e9025da3e414c45625f250b434bb6175f4d89f42
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-27 10:36:52 +0530

    add: cashbook controller to control cashbook, view, and apis

---
+++ public/scripts/controllers/cashbook-controller.js
@@ -0,0 +1,31 @@
+class CashbookController {
+  #view;
+  #cashbook;
+  #apiService;
+
+  constructor(cashbook, view, apiService) {
+    this.#view = view;
+    this.#cashbook = cashbook;
+    this.#apiService = apiService;
+  }
+
+  #addAndRender(entry) {
+    const summary = this.#cashbook.addEntry(entry).getSummary();
+    this.#view.updateSummary(summary);
+    this.#view.appendEntry(entry);
+  }
+
+  #getAndRenderEntries() {
+    this.#apiService.getAllEntries((entries) => {
+      entries.forEach((entry) => this.#addAndRender(entry));
+    });
+  }
+
+  start() {
+    this.#view.onFormSubmit((entry) => {
+      this.#apiService.sendEntry(entry, (entry) => this.#addAndRender(entry));
+    });
+
+    this.#getAndRenderEntries();
+  }
+}
