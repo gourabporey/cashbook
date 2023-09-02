aa95572a984422c95d0c054afe6a3bc7b06b6c5d
Gourab Porey <gourabporey@users.noreply.github.com>
2023-09-02 12:43:10 +0530

    add: config for paths and port

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  ENTRY_PATH: process.env.ENTRY_PATH || './data/entries.json',
+  USERS_PATH: process.env.USERS_PATH || './data/users.json',
+};
--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const EntryRepository = require('./src/repositories/entry-repository');
 const { generateId } = require('./src/id-generator');
 const UserRepository = require('./src/repositories/user-repository');
 const Encryptor = require('./src/models/encryptor');
+const { ENTRY_PATH, USERS_PATH, PORT } = require('./config');
 
 const main = () => {
-  const ENTRY_PATH = './data/entries.json';
-  const USERS_PATH = './data/users.json';
-
   const encryptor = new Encryptor(crypto);
   const entryRepository = new EntryRepository(ENTRY_PATH, fs, console);
   const userRepository = new UserRepository(USERS_PATH, fs, encryptor);
@@ -19,7 +17,6 @@ const main = () => {
   const idGenerator = generateId();
 
   const app = createApp({ entryRepository, idGenerator, userRepository });
-  const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => console.log('started listening on port', PORT));
 };
 
